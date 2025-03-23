fbfc16b993a1ab2ad0beaf9402c5eb8da3616c66
pretender-2905 <pretender2905@gmail.com>
2025-03-23 19:17:25 +0500

    2 nd commit

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+name = "ibrahim"
+age = 19
+gpa = 3.6
+is_student = True
+print(name)
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
--- variable.py
+++ variable.py
@@ -1,4 +1,27 @@
-first_name = "Muhammad"
-print(first_name)
+# variable / data types(string, integer, float, boolean)
 
+first_name = "Muhammad"
+food = "pizza"
+email = "ibrahim@gmail.com"
+age = 19
+quantity = 3
+num_of_std = 50
+price = 45.50
+# is_student = True
+is_student = False
+print(first_name)
 print(f"Hello {first_name}")
+print(f"you likes {food}")
+print(f"your email is: {email}")
+print(f"you are {age} years old")
+print(f"you are buying {quantity} items")
+print(f"your class has {num_of_std} students")
+print(f"The price is {price}$")
+print(f"are you a student?: {is_student}")
+
+#booelan
+if is_student:
+    print("you are student")
+    
+else:
+    print("you are not a student")
